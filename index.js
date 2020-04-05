736d4da5393021c0839cf8dc78b6194b377aea4b
sun-street <79538889991@ya.ru>
2020-04-06 03:15:06 +0700

    Финальная проверка

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
+var jsonObject;
+
 window.onload = () => 
 {
+    //testData();
     fetch("grounds.json")
         .then(response => response.json())
-        .then(json => console.log(json));
-
-    // var request = new XMLHttpRequest();
-    // request.open("GET", "grounds.json", false);
-    // //request.send(null)
-    // var my_JSON_object = JSON.parse(request.responseText);
-    // alert (my_JSON_object.result[0]);
-    //var obj = JSON.parse();
-    document.getElementById("select").innerHTML = "<option>Все участки</option><option>390</option>";
+        .then(json => fillSelector(json));
 };
 
-function onChangeHandler()
+function onChangeHandler(groundName)
 {
-    document.getElementById("tbody").innerHTML = "<tr><td>{{selectedGround}}</td><td>{{selectedGround.number}}</td></tr>";
+    fillTable(groundName);
 };
 
+function testData()
+{
+    var json = '{"groundsList":[{"number": "111", "phoneNumbers": ["+7-953-***-45-67", "+7-953-***-56-78"]},{"number": "222", "phoneNumbers": ["+7-913-***-11-11"]}]}';
+
+    jsonObject = JSON.parse(json);
+
+    fillSelector();
+};
+
+function fillSelector()
+{
+    var innerHtml = "<option>Все участки</option>";
+
+    for (var i = 0; i < jsonObject.groundsList.length; i++) 
+    {
+        innerHtml += '<option value="' + jsonObject.groundsList[i].number + '">' + jsonObject.groundsList[i].number + '</option>';
+    }
+    
+    document.getElementById("select").innerHTML = innerHtml;
+};
+
+function fillTable(groundName)
+{
+    var innerHtml = "";
+
+    for (var i = 0; i < jsonObject.groundsList.length; i++) 
+    {
+        if (groundName == "Все участки" || groundName == jsonObject.groundsList[i].number)
+        {
+            innerHtml += "<tr><td>" + jsonObject.groundsList[i].number + "</td><td>";
+
+            for (var j = 0; j < jsonObject.groundsList[i].phoneNumbers.length; j++) 
+            {
+                innerHtml += jsonObject.groundsList[i].phoneNumbers[j];
+
+                if(j != jsonObject.groundsList[i].phoneNumbers.length)
+                {
+                    innerHtml += "</BR>";
+                }
+            }
+
+            innerHtml += "</td></tr>";
+        }
+    }
+    
+    document.getElementById("tbody").innerHTML = innerHtml;
+};
