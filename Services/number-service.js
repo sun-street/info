d296a4b4e91deaae3b22b89ab19e719d13aadac6
sun-street <79538889991@ya.ru>
2020-04-06 19:53:55 +0700

    Завершение подготовки

--- Services/number-service.js
+++ Services/number-service.js
@@ -1,7 +1,37 @@
+function replaceAt(string, index, replace) 
+{
+    return string.substring(0, index) + replace + string.substring(index + 1);
+}
+function processNumber(number)
+{
+    number = replaceAt(number, 7,"*");
+    number = replaceAt(number, 8,"*");
+    number = replaceAt(number, 9,"*");
+    
+    return number;
+}
+
 function fillNumbers()
 {
+    const fs = require("fs");
+
     var fileContent = fs.readFileSync("Data/grounds.json", "utf8");
-    console.log(fileContent);
+    var jsonObject = JSON.parse(fileContent);
+
+    console.log(jsonObject);
+    
+    for (var i = 0; i < jsonObject.groundsList.length; i++) 
+    {
+        for (var j = 0; j < jsonObject.groundsList[i].phoneNumbers.length; j++) 
+        {
+            jsonObject.groundsList[i].phoneNumbers[j].number = processNumber(jsonObject.groundsList[i].phoneNumbers[j].number);
+        }
+    }
+
+    var newJsonObject = JSON.stringify(jsonObject, null, '\t');
+    console.log(newJsonObject);
+
+    fs.writeFileSync("Data/safe-grounds.json", newJsonObject);
 }
 
 fillNumbers();
